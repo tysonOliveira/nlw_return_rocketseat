3372da58b589d335c26414313742655c614ad281
tysonOliveira <thaisonoliveira@gmail.com>
2022-05-02 21:21:17 -0300

    Widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,5 @@
-interface ButtonProps {
-  text: string;
-}
+import { Widget } from "./components/Widget";
 
-function Button(props: ButtonProps) {
-  return <button className="bg-violet-500 px-4">{props.text ?? 'Default'}</button>
+export function App() {
+  return <Widget />
 }
-
-function App() {
-  return (
-    <div className="flex gap-2">
-      <Button text="Enviar" /> 
-      <Button text="ok" />     
-  
-    </div>
-  )
-}
-
-export default App
