558524edc8e67b9c1ef355876613412a639fb894
tysonOliveira <thaisonoliveira@gmail.com>
2022-05-04 01:33:22 -0300

    stage02

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -3,6 +3,7 @@ import bugImageUrl from '../../assets/bug.svg';
 import ideaImageUrl from '../../assets/idea.svg';
 import thoughtImageUrl from '../../assets/thought.svg';
 import { FeedbackContentStep } from './Steps/FeedbackContentStep';
+import { FeedbackSuccessStep } from './Steps/FeedbackSuccessStep';
 import { FeedbackTypeStep } from './Steps/FeedbackTypesStep';
 
 
@@ -32,27 +33,35 @@ export const feedbackTypes = {
 };
 
 export type FeedbackType = keyof typeof feedbackTypes;
-
+//teste
 export function WidgetForm() {
   const [feedBackType, setFeedBackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
 
   function handleRestartFeedback() {
+    setFeedbackSent(false);
     setFeedBackType(null);
   }
   
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      
-
-      {!feedBackType ? (
-        <FeedbackTypeStep onFeedbackTypeChanged={setFeedBackType} />
-      ) : ( 
-        <FeedbackContentStep 
-        feedbackType={feedBackType}
-        onFeedbackRestartRequested={handleRestartFeedback}
+      { feedbackSent ? (
+        <FeedbackSuccessStep onFeedbackRestartRequested={handleRestartFeedback}/>
+      ) : (
+        <>
+          {!feedBackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedBackType} />
+          ) : ( 
+            <FeedbackContentStep 
+            feedbackType={feedBackType}
+            onFeedbackRestartRequested={handleRestartFeedback}
+            onFeedbackSent={() => setFeedbackSent(true)}
         /> 
       )}
-      
+        </>
+      ) }  
+
+
       <footer className="text-xs text-neutral-400">
         Feito com ♥ pela Rocketseat <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
       </footer>
